7c4745b6996e8bc3085b7db427d0f62ddc11ffd4
devopMarkz <marcosdev2002@gmail.com>
2025-04-20 15:40:53 -0300

    Preparando projeto vuetify

--- backend/src/main/java/io/github/devopMarkz/backend/config/SecurityConfigurations.java
+++ backend/src/main/java/io/github/devopMarkz/backend/config/SecurityConfigurations.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,6 +31,7 @@ public class SecurityConfigurations {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/register").permitAll();
                     auth.anyRequest().authenticated();
---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    Aplicativo
+    <ul>
+      <li v-for="nome in estr">{{ nome }}</li>
+    </ul>
+    <router-view />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  const estr = ['Marcos', 'Sara'];
+</script>
+
+<style scoped>
+</style>
